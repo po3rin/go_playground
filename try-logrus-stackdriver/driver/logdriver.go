558f21b14b9697f228a36d074c35ed4eebeaf79f
po3rin <abctail30@gmail.com>
2018-09-28 23:40:45 +0900

    set gcp driver

--- try-logrus-stackdriver/driver/logdriver.go
+++ try-logrus-stackdriver/driver/logdriver.go
@@ -1,9 +1,13 @@
 package apilog
 
 import (
+	"context"
 	"os"
 
+	"github.com/knq/sdhook"
 	"github.com/sirupsen/logrus"
+	"golang.org/x/oauth2/google"
+	logging "google.golang.org/api/logging/v2"
 )
 
 // Log - logrus struct
@@ -13,8 +17,30 @@ func init() {
 	Log = logrus.New()
 	Log.Level = logrus.ErrorLevel
 	Log.Formatter = &logrus.TextFormatter{FullTimestamp: true, DisableColors: true}
+
 	if os.Getenv("EXECUTION_ENVIRONMENT") == "gce" {
-		Log.Out = os.Stdout
+		ctx := context.Background()
+
+		client, err := google.DefaultClient(ctx, logging.LoggingWriteScope)
+		if err != nil {
+			panic(err)
+		}
+
+		resLabels := map[string]string{
+			"project_id": os.Getenv("GCP_PROJECT_ID"),
+			"zone":       "asia-northeast1-b",
+		}
+
+		hook, err := sdhook.New(
+			sdhook.HTTPClient(client),
+			sdhook.ProjectID(os.Getenv("GCP_PROJECT_ID")),
+			sdhook.LogName("test"),
+			sdhook.Resource("gce_instance", resLabels),
+		)
+		if err != nil {
+			panic(err)
+		}
+		Log.AddHook(hook)
 		return
 	}
 	logFile, err := os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
--- try-logrus-stackdriver/handler/handler.go
+++ try-logrus-stackdriver/handler/handler.go
@@ -11,19 +11,31 @@ func AllHandler(c *gin.Context) {
 	l.Info("ii")
 	l.Warn("ww")
 	l.Error("ee")
+	c.JSON(200, gin.H{
+		"message": "ok",
+	})
 }
 
 // InfoHandler try logging all
 func InfoHandler(c *gin.Context) {
 	l.Info("ii")
+	c.JSON(200, gin.H{
+		"message": "ok",
+	})
 }
 
 // WarnHandler try logging all
 func WarnHandler(c *gin.Context) {
 	l.Warn("ee")
+	c.JSON(200, gin.H{
+		"message": "ok",
+	})
 }
 
 // ErrorHandler try logging all
 func ErrorHandler(c *gin.Context) {
 	l.Error("ee")
+	c.JSON(200, gin.H{
+		"message": "ok",
+	})
 }
