f744775481302b1b470ea06ed3f3f8bdf1eeb577
po3rin <abctail30@gmail.com>
2018-11-21 16:54:25 +0900

    add mirror

--- try-binding/main.go
+++ try-binding/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/mholt/binding"
@@ -18,12 +21,14 @@ type FieldWords []FieldWord
 
 type FieldWord struct {
 	FieldName string `json:"field_name"`
-	Word      Words  `json:"word"`
+	Word      Words  `json:"word`
 }
 
 type Words []Word
 
-type Word string
+type Word interface {
+	GetQuery(string) string
+}
 
 func (cf *ContactForm) FieldMap(req *http.Request) binding.FieldMap {
 	return binding.FieldMap{
@@ -36,13 +41,50 @@ func (cf *ContactForm) FieldMap(req *http.Request) binding.FieldMap {
 }
 
 func handler(resp http.ResponseWriter, req *http.Request) {
-	contactForm := new(ContactForm)
-	if errs := binding.Bind(req, contactForm); errs != nil {
-		http.Error(resp, errs.Error(), http.StatusBadRequest)
+	// bind word_field only
+	var buf bytes.Buffer
+	req.Body = NewTeeReadCloser(req.Body, &buf)
+	decoder := json.NewDecoder(req.Body)
+
+	var Binder struct {
+		FieldWords []struct {
+			FieldName string      `json:"field_name"`
+			Word      interface{} `json:"word"`
+		} `json:"field_words"`
+	}
+	err := decoder.Decode(&Binder)
+	if err != nil {
+		http.Error(resp, err.Error(), http.StatusBadRequest)
 		return
 	}
-	fmt.Fprintf(resp, "from:    %d\n", contactForm.User.ID)
-	fmt.Fprintf(resp, "field_words: %s\n", contactForm.FieldWords)
+	fmt.Println(Binder)
+
+	// secondly bind.
+	rawData := buf.Bytes()
+	contactForm := new(ContactForm)
+	// if errs := binding.Bind(rawData, contactForm); errs != nil {
+	// 	http.Error(resp, errs.Error(), http.StatusBadRequest)
+	// 	return
+	// }
+
+	if err := json.Unmarshal(rawData, contactForm); err != nil {
+		fmt.Println("JSON Unmarshal error:", err)
+		return
+	}
+
+	fmt.Println(contactForm)
+}
+
+type readCloser struct {
+	io.Reader
+	io.Closer
+}
+
+func NewTeeReadCloser(rc io.ReadCloser, w io.Writer) io.ReadCloser {
+	return &readCloser{
+		Reader: io.TeeReader(rc, w),
+		Closer: rc,
+	}
 }
 
 func main() {
