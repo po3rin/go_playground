10313a9cab7da7292342a34717cd42faa4b05d44
po3rin <abctail30@gmail.com>
2018-12-02 00:57:14 +0900

    add token

--- try-analysis/main.go
+++ try-analysis/main.go
@@ -7,10 +7,22 @@ import (
 	"go/token"
 )
 
+var src = `package p
+
+import x "pkg"
+
+func f() {
+    if x := x.f(); x != nil {
+        x(func(x int) int { return x + 1 })
+    }
+}
+`
+
 func main() {
 	// parseExpr()
-	parseFile()
-	// inspect()
+	// parseFile()
+	inspect()
+	// scope()
 }
 
 func parseExpr() {
@@ -22,10 +34,8 @@ func parseFile() {
 	fset := token.NewFileSet()
 	f, _ := parser.ParseFile(fset, "./example/example.go", nil, parser.Mode(0))
 
-	for _, d := range f.Imports {
-		ast.Print(fset, d.Name)
-		fmt.Printf("position of first character belonging to the node: %v", d.Pos())
-		fmt.Printf("position of first character immediately after the node: %v", d.End())
+	for _, d := range f.Decls {
+		ast.Print(fset, d)
 	}
 }
 
@@ -42,13 +52,42 @@ func inspect() {
 	// 	return true
 	// })
 
+	// ast.Inspect(f, func(n ast.Node) bool {
+	// 	if v, ok := n.(*ast.Ident); ok {
+	// 		if v.Obj != nil {
+	// 			if v.Obj.Kind == 5 {
+	// 				fmt.Println(v.Name)
+	// 			}
+	// 		}
+	// 	}
+	// 	return true
+	// })
+
 	ast.Inspect(f, func(n ast.Node) bool {
-		if v, ok := n.(*ast.Ident); ok {
-			if v.Obj != nil {
-				if v.Obj.Kind == 5 {
-					fmt.Println(v.Name)
-				}
+		if v, ok := n.(*ast.FuncDecl); ok {
+			fmt.Println(v.Name)
+			fmt.Println(fset.Position(v.Pos()))
+			// fmt.Println(v.Pos())
+		}
+		return true
+	})
+}
+
+func scope() {
+	fset := token.NewFileSet()
+	f, _ := parser.ParseFile(fset, "example.go", src, parser.Mode(0))
+
+	ast.Inspect(f, func(n ast.Node) bool {
+		if ident, ok := n.(*ast.Ident); ok && ident.Name == "x" {
+			var decl interface{}
+			if ident != nil && ident.Obj != nil {
+				decl = ident.Obj.Decl
 			}
+			var kind ast.ObjKind
+			if ident.Obj != nil {
+				kind = ident.Obj.Kind
+			}
+			fmt.Printf("%-17sobj=%-12p  kind=%s decl=%T\n", fset.Position(ident.Pos()), ident.Obj, kind, decl)
 		}
 		return true
 	})
