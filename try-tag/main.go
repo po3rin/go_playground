d5adc2c0a734e4b3a4bb112ddf2a47a34ccc976d
po3rin <abctail30@gmail.com>
2018-12-27 22:59:45 +0900

    try interface

--- try-tag/main.go
+++ try-tag/main.go
@@ -17,6 +17,7 @@ type Sample struct {
 	SliceByJSON []int          `default:"[]"` // Supports JSON format
 	MapByJSON   map[string]int `default:"{}"`
 	Str         *Str           `default:"{}"`
+	Responses   interface{}    `default:"[]struct{}"`
 }
 
 func main() {
