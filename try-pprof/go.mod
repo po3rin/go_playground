dabc93e9c9ebcfa7063d4fbe4e3c7e07f8e537a2
po3rin <abctail30@gmail.com>
2019-08-06 23:21:35 +0900

    try pprof

---
+++ try-pprof/go.mod
@@ -0,0 +1,3 @@
+module github.com/po3rin/try-pprof
+
+go 1.12
---
+++ try-pprof/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "net/http/pprof"
+)
+
+func fib(n int) int {
+	if n < 2 {
+		return n
+	}
+	return fib(n-1) + fib(n-2)
+}
+
+func main() {
+	go func() {
+		log.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
+
+	fmt.Println("start")
+	for {
+		fib(30)
+	}
+}
