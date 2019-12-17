776dc303f9d54e4164d9042c22870c23182e6bc8
po3rin <abctail30@gmail.com>
2019-12-18 08:08:55 +0900

    cas

---
+++ cas/cas.go
@@ -0,0 +1,30 @@
+package cas
+
+import (
+	"fmt"
+	"sync"
+)
+
+var lock = sync.Mutex{}
+var store = map[string]string{}
+
+func compareAndSwap(key, nextVal, currentVal string) (bool, error) {
+	lock.Lock()
+	defer lock.Unlock()
+
+	_, containsKey := store[key]
+	if !containsKey {
+		if len(currentVal) == 0 {
+			store[key] = nextVal
+			return true, nil
+		}
+		return false, fmt.Errorf("expectedval %s for key %s, but found empty", currentVal, key)
+	}
+
+	if store[key] == currentVal {
+		store[key] = nextVal
+		return true, nil
+	}
+
+	return false, nil
+}
---
+++ cas/go.mod
@@ -0,0 +1,3 @@
+module github.com/po3rin/cas
+
+go 1.13
